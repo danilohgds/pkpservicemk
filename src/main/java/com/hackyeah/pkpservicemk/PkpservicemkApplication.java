c688870f32f6e2d1bd7f6c3d89f0988bde5e9ad9
Danilo_Silva@epam.com <Danilo_Silva@epam.com>
2018-11-25 04:47:13 +0100

    pkpservice mock

--- src/main/java/com/hackyeah/pkpservicemk/PkpservicemkApplication.java
+++ src/main/java/com/hackyeah/pkpservicemk/PkpservicemkApplication.java
@@ -2,6 +2,10 @@ package com.hackyeah.pkpservicemk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class PkpservicemkApplication {
@@ -9,4 +13,15 @@ public class PkpservicemkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PkpservicemkApplication.class, args);
 	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("*");
+            }
+        };
+    }
 }

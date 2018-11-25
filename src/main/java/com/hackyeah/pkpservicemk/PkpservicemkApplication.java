3a14f01aa2a8e4608112d1401f1c9e66e876e3a1
Danilo_Silva@epam.com <Danilo_Silva@epam.com>
2018-11-25 03:50:41 +0100

    initial commit for mocks

---
+++ src/main/java/com/hackyeah/pkpservicemk/PkpservicemkApplication.java
@@ -0,0 +1,12 @@
+package com.hackyeah.pkpservicemk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkpservicemkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkpservicemkApplication.class, args);
+	}
+}
